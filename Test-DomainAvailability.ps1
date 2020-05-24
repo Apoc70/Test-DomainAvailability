9aa10785d151af7cfd7bb04ddcf602c590ab0bd2
Thomas Stensitzki <thomas@mcsmemail.de>
2020-05-24 15:56:03 +0200

    README.mdupdated

--- Test-DomainAvailability.ps1
+++ Test-DomainAvailability.ps1
@@ -1,53 +1,53 @@
 function Test-DomainAvailability {
   <#
-    .SYNOPSIS
-    Check the availability of a domain in a selected Office 365 region.  
+      .SYNOPSIS
+      Check the availability of a domain in a selected Office 365 region.  
 
-    .DESCRIPTION
-    The script queries the login uri for the selected Office 365 region. The response contains metadata about the domain queried.
+      .DESCRIPTION
+      The script queries the login uri for the selected Office 365 region. The response contains metadata about the domain queried.
     
-    If the domain already exists in the specified region the metadata contains information if the domain is verified and/or federated
+      If the domain already exists in the specified region the metadata contains information if the domain is verified and/or federated
 
-    Load function into your current PowerShell session:
-    . .\Test-DomainAvailability.ps1
+      Load function into your current PowerShell session:
+      . .\Test-DomainAvailability.ps1
 
-    .PARAMETER DomainName
-    The domain name you want to verify. Example: example.com
+      .PARAMETER DomainName
+      The domain name you want to verify. Example: example.com
 
-    .PARAMETER LookupRegion
-    The Office 365 region where you want to verify the domain.
-    Currently implemented: Global, Germany, China
+      .PARAMETER LookupRegion
+      The Office 365 region where you want to verify the domain.
+      Currently implemented: Global, Germany, China
 
-    .NOTES
+      .NOTES
 
-    Author: ?
-    (Source: https://blogs.technet.microsoft.com/tip_of_the_day/2017/02/16/cloud-tip-of-the-day-use-powershell-to-check-domain-availability-for-office-365-and-azure/)
-    Enhancement: Thomas Stensitzki
+      Author: ?
+      (Source: https://blogs.technet.microsoft.com/tip_of_the_day/2017/02/16/cloud-tip-of-the-day-use-powershell-to-check-domain-availability-for-office-365-and-azure/)
+      Enhancement: Thomas Stensitzki
 	
-    THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
-    RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+      THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
+      RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
 
-    Send comments and remarks to: support@granikos.eu
+      Send comments and remarks to: support@granikos.eu
 	
-    Version 1.0, 2017-02-17
+      Version 1.0, 2017-02-17
 
-    .LINK
-    http://scripts.Granikos.eu
+      .LINK
+      http://scripts.Granikos.eu
     
-    .EXAMPLE
-    Test-DomainAvailability -DomainName example.com 
+      .EXAMPLE
+      Test-DomainAvailability -DomainName example.com 
 
-    Test domain availability in the default region - Office 365 Global
+      Test domain availability in the default region - Office 365 Global
 
-    .EXAMPLE
-    Test-DomainAvailability -DomainName example.com -LookupRegion China
+      .EXAMPLE
+      Test-DomainAvailability -DomainName example.com -LookupRegion China
 
-    Test domain availability in Office 365 China
+      Test domain availability in Office 365 China
   #>
+
   param(
     [Parameter(Mandatory=$true,HelpMessage='A domain name (e.g. example.com) is required')]
     [string]$DomainName,
-    [Parameter()]
     [ValidateSet('Global','Germany','China')]
     [string]$LookupRegion = 'Global'
   )
